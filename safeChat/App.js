f685b375b3c9f04a3c3738242b13e8898af7ee2a
Kauana Santos <kau.grola@gmail.com>
2019-05-11 16:31:27 -0700

    add login screen frontend

--- safeChat/App.js
+++ safeChat/App.js
@@ -1,62 +1,48 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Font } from 'expo';
+import { View, Text } from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+import LoginScreen from './screens/LoginScreen';
+
+const AppNavigator = createStackNavigator({
+  Login: {screen: LoginScreen},
+}, {
+  headerLayoutPreset: 'center',
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fontLoaded: false,
+    };
+  }
+
+	async componentDidMount() {
+		await Font.loadAsync({
+			georgia: require('./assets/fonts/Georgia.ttf'),
+			regular: require('./assets/fonts/Montserrat-Regular.ttf'),
+			light: require('./assets/fonts/Montserrat-Light.ttf'),
+		});
+
+		this.setState({ fontLoaded: true });
+	}
 
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    const { fontLoaded } = this.state;
+
+    if (fontLoaded) {
       return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+        <View style={{ flex: 1 }}>
+          <AppContainer />
         </View>
       );
     }
+
+    return <Text>Loading...</Text>;
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
